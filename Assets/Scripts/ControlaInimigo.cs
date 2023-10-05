55f605d67a874faac08b78215a89e10e2e83b357
Rodrigo Carvalho <rodrigofcc.dev@gmail.com>
2023-10-04 21:13:39 -0300

    first commit

---
+++ Assets/Scripts/ControlaInimigo.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaInimigo : MonoBehaviour
+{
+
+    public GameObject Jogador;
+    public float Velocidade = 5;
+    private Rigidbody rigidbodyInimigo;
+    private Animator animatorInimigo;
+
+    void Start () {
+        Jogador = GameObject.FindWithTag("Player");
+        int geraTipoZumbi = Random.Range(1, 28);
+        transform.GetChild(geraTipoZumbi).gameObject.SetActive(true);
+        rigidbodyInimigo = GetComponent<Rigidbody>();
+        animatorInimigo = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+       float distancia = Vector3.Distance(transform.position, Jogador.transform.position);
+
+        Vector3 direcao = Jogador.transform.position - transform.position;
+
+        Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+        rigidbodyInimigo.MoveRotation(novaRotacao);
+
+        if (distancia > 2.5)
+        {
+            rigidbodyInimigo.MovePosition
+                (rigidbodyInimigo.position + 
+                direcao.normalized * Velocidade * Time.deltaTime);
+
+            animatorInimigo.SetBool("Atacando", false);            
+        }
+        else
+        {
+            animatorInimigo.SetBool("Atacando", true);
+        }
+    }
+
+     void AtacaJogador ()
+    {
+        Time.timeScale = 0;
+        Jogador.GetComponent<ControlaJogador>().TextoGameOver.SetActive(true);
+        Jogador.GetComponent<ControlaJogador>().Vivo = false;
+    }
+}
